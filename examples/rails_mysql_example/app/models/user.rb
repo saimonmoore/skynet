c2b2868dd8cf29b42ffa7abb3db8368ce87795fa
Saimon Moore <saimon@saimonmoore.net>
2008-08-26 17:34:15 +0200

    Initial commit of skynet svn repo at revision: 1903

---
+++ examples/rails_mysql_example/app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  
+  has_many :user_favorites, :dependent => :destroy
+  
+  # Moves comma seperated column into seperate relationship table
+  def migrate_favorites
+    return unless self.favorites
+    self.favorites.split(",").each do |favorite|
+      self.user_favorites.create(:favorite => favorite)
+    end
+    self.favorites = nil
+    self.save
+  end
+  
+  # Slow running welcome email sender
+  def send_welcome_email
+    sleep 5
+    UserMailer.deliver_welcome(self)
+  end
+end
+                       
